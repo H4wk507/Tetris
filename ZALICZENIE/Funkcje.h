26721f8bdc1ad7d929778d6589fd32b61a10c5c5
Piotr Skowronski <quasand89@gmail.com>
2022-01-17 17:00:32 +0100

    fix

--- ZALICZENIE/Funkcje.h
+++ ZALICZENIE/Funkcje.h
@@ -4,7 +4,7 @@
 #include "Constants.h"
 #include <random>
 
-void drawText(sf::RenderWindow& window, float x, float y, const std::string& text);
+void drawText(sf::RenderWindow& window, double x, double y, const std::wstring& text);
 void openInfo(sf::RenderWindow& window);
 void restartGame(std::vector<std::vector<int>>& i_matrix, bool& game_over, int& lines_cleared, int& level);
 void startSinglePlayer(sf::RenderWindow& window);
